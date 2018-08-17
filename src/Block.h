c0c986ba37d5010f3dc7c8662800aa232f79f5cb
Andy Shu <andyshuhsin@gmail.com>
2018-08-17 15:58:07 +0800

    Define tx and transactions and blocks

--- src/Block.h
+++ src/Block.h
@@ -1,25 +1,22 @@
 #pragma once
-//
-// Created by Andy Shu on 29/7/2018.
-//
 
 #include <stdint.h>
+#include "datatypes.h"
+#include "hash.h"
+#include "messages/tx.h"
 
-struct BlockHeader {
-    uint32_t version;
-    uint32_t previous_block_hash;
-    uint32_t merkle_root;
+#define MAX_TX_PER_BLOCK 4096
+
+struct BlockPayload {
+    int32_t version;
+    SHA256_HASH prev_block;
+    SHA256_HASH merkle_root;
     uint32_t timestamp;
-    uint32_t difficulty_target;
+    uint32_t bits;
+    uint32_t nonce;
+    uint64_t txn_count;
+    TX txns[MAX_TX_PER_BLOCK];
 };
 
-typedef struct BlockHeader BlockHeader;
-
-struct Block {
-    uint32_t magic_number;
-    uint32_t block_size;
-    BlockHeader header;
-};
-
-typedef struct Block Block;
+typedef struct BlockPayload BlockPayload;
 
---
+++ src/messages/tx.h
@@ -0,0 +1,65 @@
+#pragma once
+
+#include <stdint.h>
+#include "datatypes.h"
+#include "hash.h"
+
+#define MAX_TX_COUNT 4096
+#define MAX_SCRIPT_LENGTH 65536
+#define MAX_WITNESS_COMPONENT_DATA_LENGTH 4096
+#define MAX_WITNESS_COMPONENT_COUNT 1024
+
+#define TX int
+
+// @see https://en.bitcoin.it/wiki/Protocol_documentation#tx
+
+struct Outpoint {
+    SHA256_HASH hash;
+    uint32_t index;
+};
+
+typedef struct Outpoint Outpoint;
+
+struct TxIn {
+    Outpoint previous_output;
+    uint64_t script_length;
+    Byte signature_script[MAX_SCRIPT_LENGTH];
+    uint32_t sequence;
+};
+
+typedef struct TxIn TxIn;
+
+struct TxOut {
+    int64_t value;
+    uint64_t pk_script_length;
+    Byte pk_script[MAX_SCRIPT_LENGTH];
+};
+
+typedef struct TxOut TxOut;
+
+// @see https://bitcoin.stackexchange.com/questions/68924/
+
+struct WitnessComponent {
+    uint64_t length;
+    Byte data[MAX_WITNESS_COMPONENT_DATA_LENGTH];
+};
+
+typedef struct WitnessComponent WitnessComponent;
+
+struct TxWitness {
+    uint64_t count;
+    WitnessComponent components[MAX_WITNESS_COMPONENT_COUNT];
+};
+
+typedef struct TxWitness TxWitness;
+
+struct TxPayload {
+    int32_t version;
+    Byte flag;
+    uint64_t tx_in_count;
+    TxIn tx_in[MAX_TX_COUNT];
+    uint64_t tx_out_count;
+    TxOut tx_out[MAX_TX_COUNT];
+    TxWitness tx_witness[MAX_TX_COUNT];
+    uint32_t lock_time;
+};
