6bd9b77898d89762fe57183a545924212fd4bb37
Andy Shu <andyshuhsin@gmail.com>
2018-09-20 19:38:21 +0800

    Disable new block validation

--- src/config.c
+++ src/config.c
@@ -11,7 +11,7 @@ const struct Config config = {
         .timeoutPeers = SECOND_TO_MILLISECOND(10),
         .printNodeStatus = SECOND_TO_MILLISECOND(5),
         .ping = SECOND_TO_MILLISECOND(13),
-        .validateNewBlocks = MINUTE_TO_MILLISECOND(1),
+        .validateNewBlocks = 0,
     },
     .tolerances = {
         .handshake = SECOND_TO_MILLISECOND(10),
@@ -32,5 +32,5 @@ const struct Config config = {
     .ibdPeerMaxBlockDifference = 100,
     .apiPort = 9494,
     .silentIncomingMessageCommands = "inv,pong,ping,addr",
-    .verifyBlocks = true,
+    .verifyBlocks = false,
 };
